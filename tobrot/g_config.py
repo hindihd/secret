b8fa008a93d71398946222bc11f0c2022f435fd8
hindihd <74170281+hindihd@users.noreply.github.com>
2021-02-20 09:21:13 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,7 +2,7 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-	TG_BOT_TOKEN= "1268fill---your----data"
+	TG_BOT_TOKEN= "1268fill---your---data"
 	APP_ID = 1373938
 	API_HASH = "fill--your--data"
 	OWNER_ID = "" #ID of bot owner
